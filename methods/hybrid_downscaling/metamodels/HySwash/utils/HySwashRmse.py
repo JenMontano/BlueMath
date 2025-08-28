c6571251ecbcd0ffc798f6843e1a6d1da58375ce
Javier Tausía Hoyal <tausiaj@unican.es>
2025-08-28 11:37:44 +0000

    [JTH] little modifications to run cases properly

--- methods/hybrid_downscaling/metamodels/HySwash/utils/HySwashRmse.py
+++ methods/hybrid_downscaling/metamodels/HySwash/utils/HySwashRmse.py
@@ -1,11 +1,9 @@
-from yaml import warnings
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import xarray as xr
 from bluemath_tk.datamining.pca import PCA
 from bluemath_tk.interpolation.rbf import RBF
-import matplotlib.pyplot as plt
-
 
 
 # Divide the cases in train and test group
@@ -13,11 +11,12 @@ def shuffle_cases(cases, n_fold):
     """
     Shuffle cases for cross-validation.
     """
-    shuffled_cases = cases.copy() # Dont modify the original n_cases
-    np.random.seed(2) 
+    shuffled_cases = cases.copy()  # Dont modify the original n_cases
+    np.random.seed(2)
     np.random.shuffle(shuffled_cases)
     split_arrays = np.array_split(shuffled_cases, n_fold)
     return split_arrays
+
 
 def rmse(y_true, y_pred):
     mask = ~np.isnan(y_true) & ~np.isnan(y_pred)
@@ -31,23 +30,22 @@ def rmse(y_true, y_pred):
 
     return rmse
 
-def print_rmse( n_cases, postprocessed_output, mda , vars ):
 
+def print_rmse(n_cases, postprocessed_output, mda, vars):
     import warnings
+
     warnings.filterwarnings("ignore", message="The NumPy module was reloaded*")
-    rmse_df_list = [] 
+    rmse_df_list = []
     rmse_Ru2 = []
     folds = 5
-    n_cases_list = np.arange(50,n_cases,50)
-    rmse_dict={}
+    n_cases_list = np.arange(50, n_cases, 50)
+    rmse_dict = {}
     for var in vars:
-        rmse_dict[var]=[]
-
+        rmse_dict[var] = []
 
     for cases_i in n_cases_list:
-    
         # Select train and test data
-        cases = np.arange(0,cases_i)
+        cases = np.arange(0, cases_i)
         cases_folds = shuffle_cases(cases, 5)
 
         train_cases = np.concatenate(cases_folds[:4])
@@ -57,15 +55,15 @@ def print_rmse( n_cases, postprocessed_output, mda , vars ):
 
         train_data = postprocessed_output.isel(case_num=train_cases)
         test_data = postprocessed_output.isel(case_num=test_cases)
-        
+
         pca = {}
-        rbf = {}      
+        rbf = {}
 
         # Calculate RMSE for each variable
         for var in vars:
             if var == "Ru2":
                 # RunUp
-                Ru2=train_data[["Ru2"]].Ru2.values
+                Ru2 = train_data[["Ru2"]].Ru2.values
                 # # Convert Ru2 numpy array to DataFrame
                 rbf_Ru = RBF()
                 rbf_Ru.logger.disabled = True
@@ -74,12 +72,11 @@ def print_rmse( n_cases, postprocessed_output, mda , vars ):
                     subset_data=mda.centroids.iloc[train_data["case_num"].values, :],
                     target_data=Ru2_df,
                 )
-                reconstructed_Ru2=rbf_Ru.predict(dataset=test_data_df)
-                rmse_Ru2s=rmse(reconstructed_Ru2.Ru2.values,test_data["Ru2"].values)
+                reconstructed_Ru2 = rbf_Ru.predict(dataset=test_data_df)
+                rmse_Ru2s = rmse(reconstructed_Ru2.Ru2.values, test_data["Ru2"].values)
                 rmse_dict["Ru2"].append(rmse_Ru2s)
-            
-            else:
 
+            else:
                 # Create RBF from train data
 
                 # Get PCA components from the Hs field of the train data
@@ -100,39 +97,46 @@ def print_rmse( n_cases, postprocessed_output, mda , vars ):
                 rbf[var].fit(
                     subset_data=mda.centroids.iloc[train_data["case_num"].values, :],
                     target_data=pca[var].pcs_df,
-                    num_workers=20
+                    num_workers=20,
                 )
 
                 # Get predicted Hrms from Test data
                 # Spatial Reconstruction: Test data
-                test_data_df=mda.centroids.iloc[test_data["case_num"].values, :]
+                test_data_df = mda.centroids.iloc[test_data["case_num"].values, :]
                 pcs_test_predicted = rbf[var].predict(
                     dataset=test_data_df,
                 )
                 pcs_test_predicted_ds = xr.Dataset(
-                    {'PCs': (["case_num", "n_component"], pcs_test_predicted.values)},
+                    {"PCs": (["case_num", "n_component"], pcs_test_predicted.values)},
                     coords={
                         "case_num": (["case_num"], test_data["case_num"].values),
-                        "n_component": (["n_component"], range(1,len(pcs_test_predicted.columns)+1)),
-                    })
-                
-                reconstructed_test_data = pca[var].inverse_transform(PCs=pcs_test_predicted_ds)
+                        "n_component": (
+                            ["n_component"],
+                            range(1, len(pcs_test_predicted.columns) + 1),
+                        ),
+                    },
+                )
+
+                reconstructed_test_data = pca[var].inverse_transform(
+                    PCs=pcs_test_predicted_ds
+                )
                 rmse_values = []
                 for i in reconstructed_test_data.case_num.values:
-                    rmse_var=rmse(reconstructed_test_data.sel(case_num=i)[var].values,test_data.sel(case_num=i)[var].values)
+                    rmse_var = rmse(
+                        reconstructed_test_data.sel(case_num=i)[var].values,
+                        test_data.sel(case_num=i)[var].values,
+                    )
                     rmse_values.append(rmse_var)
                 mean_rmse = np.mean(rmse_values)
-                rmse_dict[var].append(mean_rmse) 
-                
+                rmse_dict[var].append(mean_rmse)
 
     plt.figure(figsize=(8, 5))
     for var in vars:
-        plt.plot(n_cases_list, rmse_dict[var], marker='o', label=var)
-    plt.xlabel('Number of Cases')
-    plt.ylabel('RMSE')
-    plt.title('RMSE vs Number of Cases')
+        plt.plot(n_cases_list, rmse_dict[var], marker="o", label=var)
+    plt.xlabel("Number of Cases")
+    plt.ylabel("RMSE")
+    plt.title("RMSE vs Number of Cases")
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
     plt.show()
-
